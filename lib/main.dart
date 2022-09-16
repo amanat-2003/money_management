d6c3f8c54b96fb158afdabe6ee086f2d245c8307
amanat-2003 <amanatsinghnain@gmail.com>
2022-09-16 17:24:45 +0530

    lec126 completed

--- lib/main.dart
+++ lib/main.dart
@@ -188,6 +188,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        (MediaQuery.of(context).orientation == Orientation.landscape);
     final appBar = AppBar(
       title: Text('Flutter App', style: TextStyle(fontFamily: 'OpenSans')),
       actions: [
@@ -206,31 +208,33 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Show Chart"),
-                Switch(
-                  value: _showChart,
-                  onChanged: (val) {
-                    setState(() {
-                      _showChart = val;
-                    });
-                  },
-                ),
-              ],
-            ),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Show Chart"),
+                  Switch(
+                    value: _showChart,
+                    onChanged: (val) {
+                      setState(() {
+                        _showChart = val;
+                      });
+                    },
+                  ),
+                ],
+              ),
             _showChart
                 ? Container(
                     height: (MediaQuery.of(context).size.height -
                             appBar.preferredSize.height -
-                            MediaQuery.of(context).padding.top) * 0.8,
+                            MediaQuery.of(context).padding.top) *
+                        0.8,
                     child: Chart(_recentTransactions),
                   )
                 : Container(
                     height: (MediaQuery.of(context).size.height -
-                            appBar.preferredSize.height -
-                            MediaQuery.of(context).padding.top),
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top),
                     child:
                         TransactionList(_userTransactions, _deleteTransaction),
                   )
